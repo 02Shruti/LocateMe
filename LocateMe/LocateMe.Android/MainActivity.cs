438ecd6d4d7918b04000d046aaefc54e68aff33f
Shruti Mishra <shruti.mishra@saviantconsulting.com>
2019-05-29 13:01:17 +0530

    Add project files.

---
+++ LocateMe/LocateMe.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+using Android;
+
+namespace LocateMe.Droid
+{
+    [Activity(Label = "LocateMe", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+			ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.AccessCoarseLocation, Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessLocationExtraCommands, Manifest.Permission.AccessNetworkState, Manifest.Permission.AccessWifiState }, 101);
+
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			//Xamarin.FormsMaps.Init(this, savedInstanceState);
+			Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
+			LoadApplication(new App());
+			
+		}
+    }
+}
---
+++ LocateMe/LocateMe/Page/MapPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.Xaml;
+
+namespace LocateMe.Page
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MapPage : ContentPage
+	{
+		private bool nameStackLayout;
+		private double lati;
+		private double longi;
+
+		public MapPage ()
+		{
+			InitializeComponent ();
+			popup.IsVisible = true;
+			StackNameLayout.IsVisible = false;
+			getLocation();
+			MyMap.MyLocationEnabled = true;
+			
+		}
+
+		private void CancelTapped(object sender, EventArgs e)
+		{
+			popup.IsVisible = false;
+		}
+
+		public double LatitudeLabel { get; set; }
+		public double LongitudeLabel { get; set; }
+		
+		private async void getLocation()
+		{
+			var request = new GeolocationRequest(GeolocationAccuracy.High);
+			var location = await Geolocation.GetLocationAsync(request);
+			
+			latitude.Text = location.Latitude.ToString();
+			longitute.Text = location.Longitude.ToString();
+		}
+
+		public void MyMap_MapLongClicked(object sender, Xamarin.Forms.GoogleMaps.MapLongClickedEventArgs e)
+		{
+			popup.IsVisible = false;
+			lati = e.Point.Latitude;
+			longi = e.Point.Longitude;
+			StackNameLayout.IsVisible = true;
+		}
+
+		public void SaveButtonClicked(object sender, EventArgs e)
+		{
+			var locationName = nameEntry.Text;
+			var pin = new Pin()
+			{
+				Position = new Xamarin.Forms.GoogleMaps.Position(lati,longi),
+				Label = locationName
+			};
+			MyMap.Pins.Add(pin);
+			StackNameLayout.IsVisible = false;
+		}
+	}
+}
